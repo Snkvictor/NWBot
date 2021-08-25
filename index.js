36667a5343292c9e173e3a255f35c2ab785430b0
Victor Lahiguera <victorlahiguera@gmail.com>
2021-08-25 01:06:23 -0300

    Commands and Events implementation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// Require the necessary discord.js classes
+const fs = require('fs');
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-// Login to Discord with your client's token
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.login(token);
